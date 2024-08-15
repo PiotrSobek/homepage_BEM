596a2264944d9a0a813ac6f79f6e8839ecd1bb52
Piotr Sobek <piotr.sobek@wp.pl>
2024-08-15 03:01:50 +0200

    functions added

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
-console.log("Treść artykułów na stronie została zapożyczona z: https://akademiacapoeira.pl/capoeira/");
+{
+    const welcome = () => {
+        console.log("Treść artykułów na stronie została zapożyczona z: https://akademiacapoeira.pl/capoeira/");
+    }
 
-let button = document.querySelector(".button");
-let article = document.querySelector(".article");
-let article__header = document.querySelector(".article__header");
-let subHeader = document.querySelectorAll(".article__subHeader");
-let tableCell = document.querySelectorAll(".article__tableCell");
-let tableHeader = document.querySelectorAll(".article__tableHeader");
-let box = document.querySelector(".article__box")
-let themeName = document.querySelector(".themeName");
-let body = document.querySelector(".body");
-let navigation__text = document.querySelectorAll(".navigation__text");
-let header = document.querySelector(".header");
-let aside__paragraph = document.querySelector(".aside__paragraph");
-let footer__paragraph = document.querySelector(".footer__paragraph");
+    const onChangeBackgroundClick = () => {
+        const button = document.querySelector(".button");
+        const article = document.querySelector(".article");
+        const article__header = document.querySelector(".article__header");
+        const subHeader = document.querySelectorAll(".article__subHeader");
+        const tableCell = document.querySelectorAll(".article__tableCell");
+        const tableHeader = document.querySelectorAll(".article__tableHeader");
+        const box = document.querySelector(".article__box")
+        const themeName = document.querySelector(".themeName");
+        const body = document.querySelector(".body");
+        const navigation__text = document.querySelectorAll(".navigation__text");
+        const header = document.querySelector(".header");
+        const aside__paragraph = document.querySelector(".aside__paragraph");
+        const footer__paragraph = document.querySelector(".footer__paragraph");
 
-button.addEventListener("click", () => {
-    article.classList.toggle("article__webTableChangeAll");
-    body.classList.toggle("body--dark");
-    header.classList.toggle("header--dark");
-    aside__paragraph.classList.toggle("aside--dark");
-    footer__paragraph.classList.toggle("footer--dark");
-    button.classList.toggle("button--dark")
-    article__header.classList.toggle("article__webTableChange");
-    box.classList.toggle("article__webTableChangeBox");
-    themeName.innerText = article.classList.contains("article__webTableChangeAll") ? "jasny" : "ciemny"
-    
-    subHeader.forEach((e) => {e.classList.toggle("article__webTableChange")});  
-    tableCell.forEach((e) => {e.classList.toggle("article__tableCellDark")});  
-    tableHeader.forEach((e) => {e.classList.toggle("article__tableHeaderDark")});  
-    navigation__text.forEach((e) => {e.classList.toggle("navigation--dark")});  
-});
+        article.classList.toggle("article__webTableChangeAll");
+        body.classList.toggle("body--dark");
+        header.classList.toggle("header--dark");
+        aside__paragraph.classList.toggle("aside--dark");
+        footer__paragraph.classList.toggle("footer--dark");
+        button.classList.toggle("button--dark")
+        article__header.classList.toggle("article__webTableChange");
+        box.classList.toggle("article__webTableChangeBox");
+        themeName.innerText = article.classList.contains("article__webTableChangeAll") ? "jasny" : "ciemny"
+
+        subHeader.forEach((e) => { e.classList.toggle("article__webTableChange") });
+        tableCell.forEach((e) => { e.classList.toggle("article__tableCellDark") });
+        tableHeader.forEach((e) => { e.classList.toggle("article__tableHeaderDark") });
+        navigation__text.forEach((e) => { e.classList.toggle("navigation--dark") });
+    }
+
+    const init = () => {
+        const button = document.querySelector(".button");
+        button.addEventListener("click", onChangeBackgroundClick);
+        welcome()
+    }
+
+    init()
+}
 
